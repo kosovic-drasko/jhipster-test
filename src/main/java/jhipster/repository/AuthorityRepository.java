d7d678eb004f02bcf3fb189ca8472d6937519173
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-09 08:32:21 +0200

    Initial version of jhipster generated by generator-jhipster@7.8.1

---
+++ src/main/java/jhipster/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package jhipster.repository;
+
+import jhipster.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/jhipster/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package jhipster.web.rest;
