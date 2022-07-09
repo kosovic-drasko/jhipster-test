34190d6574a7206b01ba4961c2ce9f28ad83ddb1
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-09 17:53:48 +0200

    j

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), MatSidenavModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
